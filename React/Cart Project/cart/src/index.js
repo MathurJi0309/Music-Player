9c8427da73e426b0802241bf247dcc9e40043e3e
MathurJi <anileshjjssacademy84@gmail.com>
2022-09-28 11:43:29 +0530

    add the new firebase

--- React/Cart Project/cart/src/index.js
+++ React/Cart Project/cart/src/index.js
@@ -2,6 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDwDv5HGKd8sqNeLAgeu5npv0tbzJn0ZPw",
+  authDomain: "cart-418be.firebaseapp.com",
+  projectId: "cart-418be",
+  storageBucket: "cart-418be.appspot.com",
+  messagingSenderId: "252378799031",
+  appId: "1:252378799031:web:5d968c7a5ae957c5d8e632"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
