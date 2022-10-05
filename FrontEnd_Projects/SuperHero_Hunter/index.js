d718b2f9aa5db30801bca9da9ce89797303c7657
MathurJi <anileshjjssacademy84@gmail.com>
2022-10-05 18:20:34 +0530

    add the style and list

--- FrontEnd_Projects/SuperHero_Hunter/index.js
+++ FrontEnd_Projects/SuperHero_Hunter/index.js
@@ -1,11 +1,35 @@
-$('#searchbtn').click(call);
-function call(){
-    var xhrRequest=new XMLHttpRequest();
-    xhrRequest.onload=function(){
-        console.log(xhrRequest.response);
-        var responseJSON=JSON.parse(xhrRequest.response);
-        console.log(responseJSON);
-    }
-    xhrRequest.open('get','https://www.superheroapi.com/api.php/90b839ceda08471f2e4549c79d75fd30/',true);
-    xhrRequest.send();
+var superHeroArray=[];
+var list=document.getElementById('list');
+var superHeroInput=document.getElementById('superHeroValue');
+
+var filterList=[];
+superHeroInput.addEventListener('keyup',()=>{
+    let val=superHeroInput.value.toLowerCase();
+    filterList=superHeroArray.filter((item)=>{
+        var superHeroName=item.name.toLowerCase();
+        let temp=superHeroName.substr(0,val.length);
+        if(temp==val){
+            return item;
+        }        
+    })
+    addSuperHeroToList(filterList);
+    
+})
+async function getSuperHerocharacters(){
+    var resp=await fetch('https://gateway.marvel.com/v1/public/characters?limit=100&ts=1&apikey=ed58f40fc34db4a3acfc80e1bac20ac8&hash=90b839ceda08471f2e4549c79d75fd30');
+    var data=await resp.json();
+    superHeroArray=data.data.results;
+    console.log('superHeroarray',superHeroArray);
+    addSuperHeroToList(superHeroArray);
+
 }
+function addSuperHeroToList(result){
+    list.innerHTML="";
+    result.map((item)=>{
+        var li=document.createElement('li');
+        li.innerHTML=`<p>${item.name}</p>,<img>${item.thumbnail.path+item.thumbnail.extension}</img>`;
+        list.append(li); 
+    })
+}
+
+getSuperHerocharacters();
