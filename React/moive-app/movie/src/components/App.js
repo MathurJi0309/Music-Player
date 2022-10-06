f84ad4ba248063fd23b07f4ae9e65c7fb0de6d22
MathurJi <anileshjjssacademy84@gmail.com>
2022-10-06 16:58:54 +0530

    add the subcribe

--- React/moive-app/movie/src/components/App.js
+++ React/moive-app/movie/src/components/App.js
@@ -3,7 +3,23 @@ import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 
-function App() {
+class App extends React.Component{
+  componentDidMount(){
+    const {store}=this.props;
+    store.subscribe(()=>{
+      console.log('UPDATED');
+    })
+    this.forceUpdate();
+    // make api call here
+    //now we have to use the dispatch action here
+    store.dispatch({
+      type:'ADD_MOVIES',
+      movies:data
+    })
+    console.log('STATE',this.props.store.getState());
+  }
+  render(){
+    const movies=this.props.store.getState()
   return (
     <div className="App">
       <Navbar/>
@@ -13,14 +29,16 @@ function App() {
           <div className="tab">Favourites</div>
         </div>
         <div className="list">
-          {data.map(movie =>(
-            <MovieCard movie={movie}/>
+          {movies.map((movie,index) =>(
+            <MovieCard movie={movie} key={`movies-${index}`} />// we have to give the key specific so we use it here as index and pass in data.map index with the movie 
           ))}
         </div>
 
       </div>
     </div>
   );
+  }
+  
 }
 
 export default App;
